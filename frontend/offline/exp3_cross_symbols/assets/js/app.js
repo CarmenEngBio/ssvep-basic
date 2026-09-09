c5c9cfc32b2538ff88bca1c31a3c6d155c9803be
CarmenEngBio <162128452+CarmenEngBio@users.noreply.github.com>
2026-09-09 12:35:57 +0200

    Clearing out the repository to make it clearer and more organised ahead of submitting and depositing the Bachelor's thesis.

--- frontend/offline/exp3_cross_symbols/assets/js/app.js
+++ frontend/offline/exp3_cross_symbols/assets/js/app.js
@@ -1,4 +1,4 @@
-// app_exp3.js - Initialises experiment 3 
+// app.js - Initialises experiment 3 
 //Launches flickering engine for all cells and connects the with the server thorugh the ws
 // Lanza el flickering de las 4 celdas y conecta con el servidor WebSocket
 
